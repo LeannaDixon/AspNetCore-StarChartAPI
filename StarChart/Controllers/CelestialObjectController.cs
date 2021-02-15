faded4b773431213b5aa291ab2164b5923be38f9
LeannaDixon <leannapdixon@outlook.com>
2021-02-15 14:31:01 +0000

    Add new GetById method into CelestialObjectController which returns a specific Celestial object whose Id matches the specified Id, and also sets the celestial object's Name and Salellite properties.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,33 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Name = "GetById";
+
+            var orbitalObject = _context.CelestialObjects
+                .Where(x => x.OrbitedObjectId == celestialObject.Id)
+                .FirstOrDefault();
+
+            if (orbitalObject != null)
+            {
+                celestialObject.Satellites.Add(orbitalObject);
+            }
+
+            return Ok(celestialObject);
+            
         }
     }
 }
